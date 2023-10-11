e846424bc6c6fa72d55df2f344e88622d7cf5720
bellagomesp <bellagomesp2003@gmail.com>
2023-10-11 13:41:13 -0300

    Player criado
    Precisa de alterações

--- src/Model/teste.java
+++ src/Model/teste.java
@@ -2,6 +2,6 @@ package Model;
 
 public class teste {
 	public static void main(String[] args) throws Exception{
-		System.out.println("Hello World");
+		System.out.println("Hello World!!!");
 	}
 }
