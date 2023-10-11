d3a00e50c3c48c0a1b4dbae23b97f40f06178857
bellagomesp <bellagomesp2003@gmail.com>
2023-10-11 17:43:41 -0300

    Map caminhando

--- src/Model/Map.java
+++ src/Model/Map.java
@@ -29,7 +29,7 @@ class Map {
 		Territory calgary = new Territory("Calgary", new Coordinates(82f/1024, 130f/768), new Coordinates[] { new Coordinates(0.083008f, 0.154948f), new Coordinates(0.070312f, 0.178385f), new Coordinates(0.056641f, 0.210938f), new Coordinates(0.108398f, 0.210938f), new Coordinates(0.129883f, 0.154948f) });
 		Territory california = new Territory("California", new Coordinates(82f/1024, 130f/768), new Coordinates[] { new Coordinates(0.083008f, 0.154948f), new Coordinates(0.070312f, 0.178385f), new Coordinates(0.056641f, 0.210938f), new Coordinates(0.108398f, 0.210938f), new Coordinates(0.129883f, 0.154948f) });
 		Territory groelandia = new Territory("Groelandia", new Coordinates(82f/1024, 130f/768), new Coordinates[] { new Coordinates(0.083008f, 0.154948f), new Coordinates(0.070312f, 0.178385f), new Coordinates(0.056641f, 0.210938f), new Coordinates(0.108398f, 0.210938f), new Coordinates(0.129883f, 0.154948f) });
-		Territory méxico = new Territory("México", new Coordinates(82f/1024, 130f/768), new Coordinates[] { new Coordinates(0.083008f, 0.154948f), new Coordinates(0.070312f, 0.178385f), new Coordinates(0.056641f, 0.210938f), new Coordinates(0.108398f, 0.210938f), new Coordinates(0.129883f, 0.154948f) });
+		Territory mexico = new Territory("México", new Coordinates(82f/1024, 130f/768), new Coordinates[] { new Coordinates(0.083008f, 0.154948f), new Coordinates(0.070312f, 0.178385f), new Coordinates(0.056641f, 0.210938f), new Coordinates(0.108398f, 0.210938f), new Coordinates(0.129883f, 0.154948f) });
 		Territory novaYork = new Territory("Nova York", new Coordinates(82f/1024, 130f/768), new Coordinates[] { new Coordinates(0.083008f, 0.154948f), new Coordinates(0.070312f, 0.178385f), new Coordinates(0.056641f, 0.210938f), new Coordinates(0.108398f, 0.210938f), new Coordinates(0.129883f, 0.154948f) });
 		Territory quebec = new Territory("Quebec", new Coordinates(82f/1024, 130f/768), new Coordinates[] { new Coordinates(0.083008f, 0.154948f), new Coordinates(0.070312f, 0.178385f), new Coordinates(0.056641f, 0.210938f), new Coordinates(0.108398f, 0.210938f), new Coordinates(0.129883f, 0.154948f) });
 		Territory texas = new Territory("Texas", new Coordinates(82f/1024, 130f/768), new Coordinates[] { new Coordinates(0.083008f, 0.154948f), new Coordinates(0.070312f, 0.178385f), new Coordinates(0.056641f, 0.210938f), new Coordinates(0.108398f, 0.210938f), new Coordinates(0.129883f, 0.154948f) });
@@ -77,42 +77,207 @@ class Map {
 		Territory perth = new Territory("Perth", new Coordinates(82f/1024, 130f/768), new Coordinates[] { new Coordinates(0.083008f, 0.154948f), new Coordinates(0.070312f, 0.178385f), new Coordinates(0.056641f, 0.210938f), new Coordinates(0.108398f, 0.210938f), new Coordinates(0.129883f, 0.154948f) });
 		
 		
+		africaDoSul.addNeighbor(angola);
+		africaDoSul.addNeighbor(somalia);
 		
+		angola.addNeighbor(somalia);
+		angola.addNeighbor(nigeria);
 		
+		argelia.addNeighbor(italia);
+		argelia.addNeighbor(espanha);
+		argelia.addNeighbor(nigeria);
+		argelia.addNeighbor(egito);
 		
-		angola.addNeighbour(somalia);
-		angola.addNeighbour(nigeria);
-		angola.addNeighbour(africaDoSul);
+		egito.addNeighbor(nigeria);
+		egito.addNeighbor(somalia);
+		egito.addNeighbor(romenia);
+		egito.addNeighbor(jordania);
 		
-		nigeria.addNeighbour(argelia);
-		nigeria.addNeighbour(brasil);
-		
-		egito.addNeighbour(argelia);
-		egito.addNeighbour(nigeria);
-		egito.addNeighbour(romenia);
-		egito.addNeighbour(jordania);
+		nigeria.addNeighbor(somalia);
+		nigeria.addNeighbor(brasil);
 
-		somalia.addNeighbour(nigeria);
-		somalia.addNeighbour(egito);
-		somalia.addNeighbour(arabiaSaudita);
-		somalia.addNeighbour(africaDoSul);
+		somalia.addNeighbor(arabiaSaudita);
 		
-		argelia.addNeighbour(italia);
-		argelia.addNeighbour(espanha);
-		
-		
-
 		map.continents[0] = new Continent("Africa", new Territory[] {
-				argelia, egito,
-				nigeria, somalia,
-				angola, africaDoSul
-		}, 3, new Color(101, 45, 144));
+				africaDoSul,angola, argelia, egito, nigeria, somalia
+		}, 3);
+		
+		
+		alasca.addNeighbor(calgary);
+		alasca.addNeighbor(vancouver);
+		alasca.addNeighbor(siberia);
+		
+		calgary.addNeighbor(vancouver);
+		calgary.addNeighbor(groelandia);
+		
+		california.addNeighbor(vancouver);
+		california.addNeighbor(texas);
+		california.addNeighbor(mexico);
+		
+		groelandia.addNeighbor(quebec);
+		groelandia.addNeighbor(reinoUnido);
+		
+		mexico.addNeighbor(texas);
+		mexico.addNeighbor(venezuela);
+		
+		novaYork.addNeighbor(texas);
+		novaYork.addNeighbor(quebec);
+
+		quebec.addNeighbor(texas);
+		quebec.addNeighbor(vancouver);
+		
+		texas.addNeighbor(vancouver);
 		
 		
 		
+		map.continents[1] = new Continent("America do Norte", new Territory[] {
+				alasca, calgary, california, groelandia, mexico, novaYork, quebec, texas, vancouver
+		}, 5);
+		
+		
+		arabiaSaudita.addNeighbor(iraque);
+		arabiaSaudita.addNeighbor(jordania);
+		
+		bangladesh.addNeighbor(tailandia);
+		bangladesh.addNeighbor(india);
+		bangladesh.addNeighbor(coreiaDoSul);
+		bangladesh.addNeighbor(indonesia);
+		
+		cazaquistao.addNeighbor(japao);
+		cazaquistao.addNeighbor(siberia);
+		cazaquistao.addNeighbor(russia);
+		cazaquistao.addNeighbor(mongolia);
+		cazaquistao.addNeighbor(china);
+		cazaquistao.addNeighbor(turquia);
+		cazaquistao.addNeighbor(letonia);
+		
+		china.addNeighbor(mongolia);
+		china.addNeighbor(coreiaDoNorte);
+		china.addNeighbor(coreiaDoSul);
+		china.addNeighbor(india);
+		china.addNeighbor(paquistao);
+		china.addNeighbor(turquia);
+		
+		coreiaDoNorte.addNeighbor(japao);
+		coreiaDoNorte.addNeighbor(coreiaDoSul);
+		
+		coreiaDoSul.addNeighbor(tailandia);
+		coreiaDoSul.addNeighbor(india);
+		
+		estonia.addNeighbor(russia);
+		estonia.addNeighbor(letonia);
+		estonia.addNeighbor(suecia);
+		
+		india.addNeighbor(paquistao);
+		india.addNeighbor(indonesia);
+		
+		ira.addNeighbor(paquistao);
+		ira.addNeighbor(siria);
+		ira.addNeighbor(iraque);
+		
+		iraque.addNeighbor(siria);
+		iraque.addNeighbor(jordania);
+		
+		japao.addNeighbor(mongolia);
+		
+		jordania.addNeighbor(siria);
+		
+		
+		letonia.addNeighbor(russia);
+		letonia.addNeighbor(turquia);
+		letonia.addNeighbor(ucrania);
+		letonia.addNeighbor(polonia);
+		letonia.addNeighbor(suecia);
+		
+		paquistao.addNeighbor(turquia);
+		paquistao.addNeighbor(siria);
+		
+		russia.addNeighbor(siberia);
+		
+		siria.addNeighbor(turquia);
+		
+		turquia.addNeighbor(ucrania);
+		
+		map.continents[2] = new Continent("Asia", new Territory[] {
+				arabiaSaudita, bangladesh, cazaquistao, china, coreiaDoNorte, coreiaDoSul, 
+				estonia, india, ira, iraque, japao, jordania, letonia, mongolia, paquistao, 
+				russia, siberia, siria, tailandia, turquia  
+		}, 7);
 		
 		
 		
+		argentina.addNeighbor(peru);
+		argentina.addNeighbor(brasil);
+		
+		brasil.addNeighbor(peru);
+		brasil.addNeighbor(venezuela);
+		
+		peru.addNeighbor(venezuela);
+		
+		map.continents[3] = new Continent("America do Sul", new Territory[] {
+				argentina, brasil, peru, venezuela
+		}, 2);
+		
+		
+		espanha.addNeighbor(franca);
+		
+		franca.addNeighbor(italia);
+		franca.addNeighbor(suecia);
+		franca.addNeighbor(reinoUnido);
+		
+		italia.addNeighbor(romenia);
+		italia.addNeighbor(polonia);
+		italia.addNeighbor(suecia);
+		
+		polonia.addNeighbor(ucrania);
+		polonia.addNeighbor(romenia);
+		
+		romenia.addNeighbor(ucrania);
+		
+		map.continents[4] = new Continent("Europa", new Territory[] {
+				espanha, franca, italia, polonia, reinoUnido, romenia, suecia, ucrania
+		}, 5);
+		
+		australia.addNeighbor(perth);
+		australia.addNeighbor(indonesia);
+		australia.addNeighbor(novaZelandia);
+		
+
+		map.continents[5] = new Continent("Oceania", new Territory[] {
+				indonesia, novaZelandia,
+				australia, perth
+		}, 2);
+				
+		return map;
+		
+	}
+	
+	public Territory findTerritory(String name) {
+	    for (Continent continent : continents) {
+	        Territory territory = continent.findTerritory(name);
+	        if (territory != null) {
+	            return territory;
+	        }
+	    }
+	    return null;
+	}
+	
+	public int getNumberTerritory() {
+	    int count = 0;
+	    for (int i = 0; i < continents.length; ++i) {
+			count += continents[i].getNumberTerritory();
+		}
+	    return count;
+	}
+	
+	
+	public Continent findContinent(String name) {
+	    for (Continent continent : continents) {
+	        if (continent.getName().equals(name)) {
+	            return continent;
+	        }
+	    }
+	    return null;
 	}
 	
 	
