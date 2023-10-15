8319f5bd2aa0316cc25477fbcfc06d6af61846c6
Guilherme Gomes <guilhermerrg15@gmail.com>
2023-10-15 18:28:28 -0300

    ajuste nos testes

--- src_test/Model/ObjectiveCardTest.java
+++ src_test/Model/ObjectiveCardTest.java
@@ -7,16 +7,16 @@ public class ObjectiveCardTest {
 
     @Test
     public void testObjectiveCardInitialization() {
-        ObjectiveCard objectiveCard = new ObjectiveCard("Conquistar a Europa", "Europe");
+        ObjectiveCard objectiveCard = new ObjectiveCard("Conquistar 24 territórios a sua escolha", "Território Alvo");
 
-        assertEquals("Conquer Europe", objectiveCard.getDescription());
+        assertEquals("Conquistar 24 territórios a sua escolha", objectiveCard.getDescription());
         assertFalse(objectiveCard.isCompleted());
-        assertEquals("Europe", objectiveCard.getTarget());
+        assertEquals("Território Alvo", objectiveCard.getTarget());
     }
 
     @Test
     public void testSetCompleted() {
-        ObjectiveCard objectiveCard = new ObjectiveCard("Conquistar a Europa", "Europe");
+        ObjectiveCard objectiveCard = new ObjectiveCard("Conquistar na totalidade a Asia e a Africa", "Território alvo");
 
         assertFalse(objectiveCard.isCompleted());
 
