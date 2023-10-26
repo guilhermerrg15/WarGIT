059039968184b5cc603d7ca96b4b53361950334d
Guilherme Gomes <guilhermerrg15@gmail.com>
2023-10-26 13:19:59 -0300

    janela inicial adicionada

--- src/Model/teste.java
+++ src/Model/teste.java
@@ -1,8 +1,13 @@
 package Model;
+import View.JanelaInicial;
+import javax.swing.JFrame;
 
 public class teste {
 	public static void main(String[] args) throws Exception{
 		System.out.println("Hello World!!!");
+		
+		JFrame test = new JanelaInicial();
+		test.setVisible(true);
 	}
 }
 
---
+++ src/View/JanelaInicial.java
@@ -0,0 +1,56 @@
+package View;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Graphics;
+import java.awt.GridLayout;
+import java.awt.Image;
+import java.awt.Point;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class JanelaInicial extends JFrame {
+	public JanelaInicial(){
+		super("Detetive");
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setResizable(false);
+		JPanel newp = new JPanel() {
+			 @Override
+			    public void paintComponent(Graphics g) {
+			        super.paintComponent(g);
+			        Image i;
+					try {
+						i = ImageIO.read(new File("resources/imagens/bgconfiguracao.png"));
+						g.drawImage(i,0, 0, 1200, 700, null); 
+					}
+					
+					catch(IOException e){
+						System.out.println(e.getMessage());
+						System.exit(1);
+					}
+			    }
+			
+		};
+		newp.setLayout(null);
+		newp.setBackground(Color.BLACK);
+		newp.setPreferredSize(new Dimension(400,500));
+		add (newp);
+		setLocationRelativeTo(null);
+		setBounds(0,0,1200,700);
+
+		JButton comecar = new JButton("Novo jogo");
+		comecar.setBounds(200,270,120,30);
+		newp.add(comecar);
+		JButton carregar = new JButton("Carregar jogo");
+		carregar.setBounds(200,330,120,30);
+		newp.add(carregar);
+		setVisible(true);
+	}
+
+}
