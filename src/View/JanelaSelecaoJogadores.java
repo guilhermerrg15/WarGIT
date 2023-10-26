512036c0bfa120b03f7039fc874cc2bc71e0996f
Guilherme Gomes <guilhermerrg15@gmail.com>
2023-10-26 17:39:35 -0300

    Janela inicial adicionada, ainda tem que ser implementada a view para selecao de jogadores

---
+++ src/View/JanelaSelecaoJogadores.java
@@ -0,0 +1,13 @@
+package View;
+
+import Controller.*;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import Model.PlayerColor;
+
+public class JanelaSelecaoJogadores extends JFrame {
+    //View pra botar o nome dos jogadores, qual a cor de cada um e botao para iniciar o jogo de fato
+}
+
