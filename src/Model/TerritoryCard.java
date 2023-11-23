d4993df768195dcb06a9beeaef0db3a575b3d6c1
bellagomesp <bellagomesp2003@gmail.com>
2023-11-23 17:39:13 -0300

    terrytoryCard e TerritoryCardDeck quase prontas
    falta a doCardTrade e evaluateCardTrade na TerritoryCardDeck

--- src/Model/TerritoryCard.java
+++ src/Model/TerritoryCard.java
@@ -6,51 +6,16 @@ package Model;
  */
 class TerritoryCard {
 	String name;
-    private int id;
-    Territory territory;
     private String shape;
 
     /**
-     * 
      * Cria uma nova carta de território com o nome e a forma fornecidos.
      * @param name O nome da carta de território.
      * @param shape A forma da carta de território.
-     * @param id de cada carta 
      */
-    public TerritoryCard(String name, String shape, int id) {
+    public TerritoryCard(String name, String shape) {
         this.name = name;
         this.shape = shape;
-        this.id = id;
-    }
-
-    /**
-     * Define o território associado à carta.
-     * @param territory O território a ser associado à carta.
-     */
-    void setTerritory(Territory territory) {
-        this.territory = territory;
-    }
-    /**
-     * Retorna o território associado à carta com o nome especificado.
-     * @param cards A lista de cartas de território.
-     * @param carta O nome da carta cujo território deve ser retornado.
-     * @return O território associado à carta com o nome especificado, ou null se não encontrado.
-     */
-    static Territory retornaTerritory(TerritoryCard[] cards, String carta) {
-        for (TerritoryCard cart : cards) {
-            if (cart.name.equals(carta)) {
-                return cart.territory;
-            }
-        }
-        return null;
-    }
-
-    /**
-     * Retorna o ID da carta de território.
-     * @return O ID da carta de território.
-     */
-    public int getId() {
-        return id;
     }
 
     /**
@@ -68,23 +33,4 @@ class TerritoryCard {
     public String getShape() {
         return shape;
     }
-
-    /**
-     * Retorna o território associado à carta de território.
-     * @return O território associado à carta de território.
-     */
-    public Territory getTerritory() {
-        return territory;
-    }
-
-    /**
-     * Converte a carta de território para uma representação de string.
-     * @return Uma representação de string da carta de território.
-     */
-    @Override
-    public String toString() {
-        return name;
-    }
 }
-
-
--- src/Model/TerritoryCardDeck.java
+++ src/Model/TerritoryCardDeck.java
@@ -11,28 +11,14 @@ import java.util.List;
  */
 class TerritoryCardDeck {
     private static TerritoryCard[] cards;
-    private static TerritoryCardDeck territorySingleton;
-
+    private int maximumCards;
 
     /**
      * Construtor privado para impor o padrão singleton e inicializar o baralho.
      */
     private TerritoryCardDeck() {
-    	setCartas();
-        embaralhaCards();
-    }
-
-    
-    /**
-     * Obtém a instância singleton de TerritoryCardDeck.
-     *
-     * @return A instância singleton.
-     */
-    public static TerritoryCardDeck getInstance() {
-        if (territorySingleton == null) {
-            territorySingleton = new TerritoryCardDeck();
-        }
-        return territorySingleton;
+    	setShuffleCards();
+        maximumCards = 5;
     }
     
 
@@ -44,67 +30,67 @@ class TerritoryCardDeck {
     private TerritoryCard[] setCartas() {
         ArrayList<TerritoryCard> cardList = new ArrayList<>();
         
-        int idCounter = 0;
-        
-        cardList.add(new TerritoryCard("Africa do Sul", "triangulo", idCounter++));
-        cardList.add(new TerritoryCard("Angola", "quadrado", idCounter++));
-        cardList.add(new TerritoryCard("Argelia", "circulo", idCounter++));
-        cardList.add(new TerritoryCard("Egito", "triangulo", idCounter++));
-        cardList.add(new TerritoryCard("Nigeria", "circulo", idCounter++));
-        cardList.add(new TerritoryCard("Somalia", "quadrado", idCounter++));
+        cardList.add(new TerritoryCard("Africa do Sul", "triangulo"));
+        cardList.add(new TerritoryCard("Angola", "quadrado"));
+        cardList.add(new TerritoryCard("Argelia", "circulo"));
+        cardList.add(new TerritoryCard("Egito", "triangulo"));
+        cardList.add(new TerritoryCard("Nigeria", "circulo"));
+        cardList.add(new TerritoryCard("Somalia", "quadrado"));
 		
-        cardList.add(new TerritoryCard("Alasca", "triangulo", idCounter++));
-        cardList.add(new TerritoryCard("Calgary", "circulo", idCounter++));
-        cardList.add(new TerritoryCard("California", "quadrado", idCounter++));
-        cardList.add(new TerritoryCard("Groenlandia", "circulo", idCounter++));
-        cardList.add(new TerritoryCard("Mexico", "quadrado", idCounter++));
-        cardList.add(new TerritoryCard("Nova Iorque", "quadrado", idCounter++));
-        cardList.add(new TerritoryCard("Quebec", "circulo", idCounter++));
-        cardList.add(new TerritoryCard("Texas", "triangulo", idCounter++));
-        cardList.add(new TerritoryCard("Vancouver", "triangulo", idCounter++));
+        cardList.add(new TerritoryCard("Alasca", "triangulo"));
+        cardList.add(new TerritoryCard("Calgary", "circulo"));
+        cardList.add(new TerritoryCard("California", "quadrado"));
+        cardList.add(new TerritoryCard("Groenlandia", "circulo"));
+        cardList.add(new TerritoryCard("Mexico", "quadrado"));
+        cardList.add(new TerritoryCard("Nova Iorque", "quadrado"));
+        cardList.add(new TerritoryCard("Quebec", "circulo"));
+        cardList.add(new TerritoryCard("Texas", "triangulo"));
+        cardList.add(new TerritoryCard("Vancouver", "triangulo"));
 		
+        cardList.add(new TerritoryCard("Arabia Saudita", "circulo"));
+        cardList.add(new TerritoryCard("Bangladesh", "circulo"));
+        cardList.add(new TerritoryCard("Cazaquistao", "circulo"));
+        cardList.add(new TerritoryCard("China", "quadrado"));
+        cardList.add(new TerritoryCard("Coreia do Norte", "quadrado"));
+        cardList.add(new TerritoryCard("Coreia do Sul", "triangulo"));
+        cardList.add(new TerritoryCard("Estonia", "circulo"));
+        cardList.add(new TerritoryCard("India", "triangulo"));
+        cardList.add(new TerritoryCard("Ira", "quadrado"));
+        cardList.add(new TerritoryCard("Iraque", "triangulo"));
+        cardList.add(new TerritoryCard("Japao", "circulo"));
+        cardList.add(new TerritoryCard("Jordania", "quadrado"));
+        cardList.add(new TerritoryCard("Letonia", "quadrado"));
+        cardList.add(new TerritoryCard("Mongolia", "triangulo"));
+        cardList.add(new TerritoryCard("Paquistao", "circulo"));
+        cardList.add(new TerritoryCard("Russia", "triangulo"));
+        cardList.add(new TerritoryCard("Siberia", "quadrado"));
+        cardList.add(new TerritoryCard("Siria", "quadrado"));
+        cardList.add(new TerritoryCard("Tailandia", "triangulo"));
+        cardList.add(new TerritoryCard("Turquia", "triangulo"));
 		
-        cardList.add(new TerritoryCard("Arabia Saudita", "circulo", idCounter++));
-        cardList.add(new TerritoryCard("Bangladesh", "circulo", idCounter++));
-        cardList.add(new TerritoryCard("Cazaquistao", "circulo", idCounter++));
-        cardList.add(new TerritoryCard("China", "quadrado", idCounter++));
-        cardList.add(new TerritoryCard("Coreia do Norte", "quadrado", idCounter++));
-        cardList.add(new TerritoryCard("Coreia do Sul", "triangulo", idCounter++));
-        cardList.add(new TerritoryCard("Estonia", "circulo", idCounter++));
-        cardList.add(new TerritoryCard("India", "triangulo", idCounter++));
-        cardList.add(new TerritoryCard("Ira", "quadrado", idCounter++));
-        cardList.add(new TerritoryCard("Iraque", "triangulo", idCounter++));
-        cardList.add(new TerritoryCard("Japao", "circulo", idCounter++));
-        cardList.add(new TerritoryCard("Jordania", "quadrado", idCounter++));
-        cardList.add(new TerritoryCard("Letonia", "quadrado", idCounter++));
-        cardList.add(new TerritoryCard("Mongolia", "triangulo", idCounter++));
-        cardList.add(new TerritoryCard("Paquistao", "circulo", idCounter++));
-        cardList.add(new TerritoryCard("Russia", "triangulo", idCounter++));
-        cardList.add(new TerritoryCard("Siberia", "quadrado", idCounter++));
-        cardList.add(new TerritoryCard("Siria", "quadrado", idCounter++));
-        cardList.add(new TerritoryCard("Tailandia", "triangulo", idCounter++));
-        cardList.add(new TerritoryCard("Turquia", "triangulo", idCounter++));
+        cardList.add(new TerritoryCard("Argentina", "quadrado"));
+        cardList.add(new TerritoryCard("Brasil", "circulo"));
+        cardList.add(new TerritoryCard("Peru", "triangulo"));
+        cardList.add(new TerritoryCard("Venezuela", "triangulo"));
 		
-        cardList.add(new TerritoryCard("Argentina", "quadrado", idCounter++));
-        cardList.add(new TerritoryCard("Brasil", "circulo", idCounter++));
-        cardList.add(new TerritoryCard("Peru", "triangulo", idCounter++));
-        cardList.add(new TerritoryCard("Venezuela", "triangulo", idCounter++));
+        cardList.add(new TerritoryCard("Espanha", "circulo"));
+        cardList.add(new TerritoryCard("Franca", "triangulo"));
+        cardList.add(new TerritoryCard("Italia", "quadrado"));
+        cardList.add(new TerritoryCard("Polonia", "triangulo"));
+        cardList.add(new TerritoryCard("Reino Unido", "circulo"));
+        cardList.add(new TerritoryCard("Romenia", "triangulo"));
+        cardList.add(new TerritoryCard("Suecia", "quadrado"));
+        cardList.add(new TerritoryCard("Ucrania", "circulo"));
 		
-        cardList.add(new TerritoryCard("Espanha", "circulo", idCounter++));
-        cardList.add(new TerritoryCard("Franca", "triangulo", idCounter++));
-        cardList.add(new TerritoryCard("Italia", "quadrado", idCounter++));
-        cardList.add(new TerritoryCard("Polonia", "triangulo", idCounter++));
-        cardList.add(new TerritoryCard("Reino Unido", "circulo", idCounter++));
-        cardList.add(new TerritoryCard("Romenia", "triangulo", idCounter++));
-        cardList.add(new TerritoryCard("Suecia", "quadrado", idCounter++));
-        cardList.add(new TerritoryCard("Ucrania", "circulo", idCounter++));
-		
-        cardList.add(new TerritoryCard("Australia", "triangulo", idCounter++));
-        cardList.add(new TerritoryCard("Indonesia", "triangulo", idCounter++));
-        cardList.add(new TerritoryCard("Nova Zelandia", "quadrado", idCounter++));
-        cardList.add(new TerritoryCard("Perth", "circulo", idCounter++));
-        
-        
+        cardList.add(new TerritoryCard("Australia", "triangulo"));
+        cardList.add(new TerritoryCard("Indonesia", "triangulo"));
+        cardList.add(new TerritoryCard("Nova Zelandia", "quadrado"));
+        cardList.add(new TerritoryCard("Perth", "circulo"));
+
+        // Cartas coringa
+        cardList.add(new TerritoryCard("Coringa 1", "coringa"));
+        cardList.add(new TerritoryCard("Coringa 2", "coringa"));
+
 
         cards = cardList.toArray(new TerritoryCard[0]);
         return cards;
@@ -124,11 +110,11 @@ class TerritoryCardDeck {
      *
      * @return O array embaralhado de objetos TerritoryCard.
      */
-    public TerritoryCard[] setCartasEmbaralhadas() {
+    public TerritoryCard[] setShuffleCards() {
         if (cards == null) {
             setCartas();
         }
-        embaralhaCards();
+        shuffleCards();
         return cards;
     }
 
@@ -137,10 +123,45 @@ class TerritoryCardDeck {
      *
      * @return O array embaralhado de objetos TerritoryCard.
      */
-    public TerritoryCard[] embaralhaCards() {
+    public TerritoryCard[] shuffleCards() {
         List<TerritoryCard> cartaList = new ArrayList<>(Arrays.asList(cards));
         Collections.shuffle(cartaList);
         cards = cartaList.toArray(new TerritoryCard[0]);
         return cards;
     }
+
+    // Pegar uma carta após o fim de jogada com conquista
+    public void pickRandomCard(Player player) {
+        int count = 0;
+
+        while(cards[count] == null) {
+            count++;
+        }
+        player.addCard(cards[count]);
+        cards[count] = null;
+    }
+
+    // Retorna uma carta ao deck 
+    public void returnCard(TerritoryCard card) {
+        for (int i = 0; i < cards.length; i++) {
+            if (cards[i] == null) {
+                cards[i] = card;
+                return;  // Se encontrou uma posição nula, adicionou e saiu do método
+            }
+        }
+    }
+
+    //maximo de cartas por jogador 
+    public int getMaximumCards() {
+        return this.maximumCards;
+    }
+
+    // public int doCardTrade(){
+    //     return 0;
+    // }
+
+
+    // public boolean evaluateCardTrade() {
+
+    // }
 }
