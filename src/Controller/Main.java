1a55d89fac4e518d032db80d7986af42e398296a
Guilherme Gomes <guilhermerrg15@gmail.com>
2023-12-05 11:12:00 -0300

    Main na controller pra rodar

---
+++ src/Controller/Main.java
@@ -0,0 +1,10 @@
+package Controller;
+import View.*;
+import Model.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Window.getWindow();
+        API.getInstance();
+    }
+}
